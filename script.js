638f030e41ea13fb803a0cfde18f6fcb1db56038
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2025-02-03 17:54:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+
+let allNotes = {
+    'notesTitles': ["Ba", "Aufgabe"],
+    'notes': ["Banana", "Hausaufgaben machen"],
+    'archivedNotesTitles': [],
+    'archivedNotes': [],
+    'trashNotesTitles': [],
+    'trashNotes': []
+}
+
+function init() {
+    getFromLocalStorage();
+    renderNotes();
+    renderArchivedNotes();
+    renderTrashNotes();
+}
+
+function saveToLocalStorage(){
+    localStorage.setItem("allNotes", JSON.stringify(allNotes));
+}
+
+function getFromLocalStorage() {
+    let myAllNotes = JSON.parse(localStorage.getItem("allNotes"));
+    if(myAllNotes != null) {
+        allNotes = myAllNotes;        
+    }
+}
+
+function renderAllNotes() {
+    renderNotes();
+    renderArchivedNotes();
+    renderTrashNotes();
+}
+
+function renderNotes() {
+    let contentRef = document.getElementById("notes_content");
+    contentRef.innerHTML = "";
+
+    for (let indexNote = 0; indexNote < allNotes.notes.length; indexNote++) {
+        contentRef.innerHTML += getNoteTemplate(indexNote);
+    }
+}
+
+function renderArchivedNotes() {
+    let archivedContentRef = document.getElementById("archived_content");
+    archivedContentRef.innerHTML = "";
+
+    for (let indexArchivedNote = 0; indexArchivedNote < allNotes.archivedNotes.length; indexArchivedNote++) {
+        archivedContentRef.innerHTML += getArchivedNoteTemplate(indexArchivedNote);
+    }
+}
+
+function renderTrashNotes() {
+    let trashContentRef = document.getElementById("trash_content");
+    trashContentRef.innerHTML = "";
+
+    for (let indexTrashNote = 0; indexTrashNote < allNotes.trashNotes.length; indexTrashNote++) {
+        trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
+    }
+}
+
+function addNote() {
+    let noteInputRef = document.getElementById("note_input");
+    let noteInput = noteInputRef.value;
+
+    let noteTitleInputRef = document.getElementById("note_title_input");
+    let noteTitleInput = noteTitleInputRef.value;
+    
+    if(noteInput != "" && noteTitleInput != "") {
+        allNotes.notes.push(noteInput);
+        allNotes.notesTitles.push(noteTitleInput);
+
+        noteInputRef.value = "";
+        noteTitleInputRef.value = "";
+    }
+
+    saveToLocalStorage();
+
+    renderAllNotes()
+}
+
+function moveNote(indexNote, startKey, destinationKey) {
+    let noteRef = allNotes[startKey].splice(indexNote, 1);
+    allNotes[destinationKey].push(noteRef[0]);
+
+    let noteTitleRef = allNotes[startKey + "Titles"].splice(indexNote, 1);
+    allNotes[destinationKey + "Titles"].push(noteTitleRef[0]);
+
+    saveToLocalStorage();
+
+    renderAllNotes()
+}
+
+function deleteNote(indexTrashNote) {
+    allNotes.trashNotes.splice(indexTrashNote, 1);
+    allNotes.trashNotesTitles.splice(indexTrashNote, 1);
+
+    saveToLocalStorage();
+
+    renderAllNotes()
+}
+
---
+++ scripts/template.js
@@ -0,0 +1,55 @@
+
+
+function getNoteTemplate(indexNote) {
+    return `
+        <div  class="note">
+            <h3>${allNotes.notesTitles[indexNote]}</h3>
+            <p>${allNotes.notes[indexNote]}</p>
+            <div>
+                <button onclick="moveNote(${indexNote}, 'notes', 'archivedNotes')" class="btn bg_green">
+                    <img src="./assets/icons/archive.svg" alt="archive">
+                </button>
+                <button onclick="moveNote(${indexNote}, 'notes', 'trashNotes')" class="btn bg_red">
+                    <img src="./assets/icons/trash.svg" alt="trash">
+                </button>
+            </div>
+        </div>
+        `;
+}
+
+
+function getArchivedNoteTemplate(indexArchivedNote) {
+    return  `
+        <div  class="note">
+            <h3>${allNotes.archivedNotesTitles[indexArchivedNote]}</h3>
+            <p>${allNotes.archivedNotes[indexArchivedNote]}</p>
+            <div>
+                <button onclick="moveNote(${indexArchivedNote}, 'archivedNotes', 'notes')" class="btn bg_yellow">
+                    <img src="./assets/icons/note.svg" alt="note">
+                </button>
+                <button onclick="moveNote(${indexArchivedNote}, 'archivedNotes', 'trashNotes')" class="btn bg_red">
+                    <img src="./assets/icons/trash.svg" alt="trash">
+                </button>
+            </div>
+        </div>
+        `;
+}
+
+
+function getTrashNoteTemplate(indexTrashNote) {
+    return  `
+        <div  class="note">
+            <h3>${allNotes.trashNotesTitles[indexTrashNote]}</h3>
+            <p>${allNotes.trashNotes[indexTrashNote]}</p>
+            <div>
+                <button onclick="moveNote(${indexTrashNote}, 'trashNotes', 'notes')" class="btn bg_yellow">
+                    <img src="./assets/icons/note.svg" alt="note">
+                </button>
+                <button onclick="deleteNote(${indexTrashNote})" class="btn bg_red">
+                    <img src="./assets/icons/trash.svg" alt="trash">
+                </button>
+            </div>
+        </div>
+        
+        `;
+}
